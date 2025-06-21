77f2a0a144192107a79bccc3a993a17ee7eeb649
Marcelo Ribeiro <msribeiro2010@icloud.com>
2025-06-21 10:28:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,14 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <BrowserRouter basename="/marcelo-s-ribeiro-stellar-ai">
+  <Routes>
+    <Route path="/" element={<Index />} />
+    {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+    <Route path="*" element={<NotFound />} />
+  </Routes>
+</BrowserRouter>
+
     </TooltipProvider>
   </QueryClientProvider>
 );
